6dd91aa13b51ec39d5b46c25abf3512c9449dde7
yymin1022 <yymin1022@gmail.com>
2026-01-26 22:59:13 +0900

    Added Hilt AndroidEntrypoint Class

--- app/src/main/java/com/a6w/memo/MainActivity.kt
+++ app/src/main/java/com/a6w/memo/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.a6w.memo.ui.theme.LocationMemoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/a6w/memo/MepmoApplication.kt
@@ -0,0 +1,7 @@
+package com.a6w.memo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MepmoApplication: Application()
