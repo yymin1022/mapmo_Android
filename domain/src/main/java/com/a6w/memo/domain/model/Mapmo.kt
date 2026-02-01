9fca7a4ab26d07be731b8d18ef570d8ef3c181fd
Lim-shweety <smile70200@naver.com>
2026-02-01 17:57:04 +0900

    Update Mapmo.kt : Add comments

--- domain/src/main/java/com/a6w/memo/domain/model/Mapmo.kt
+++ domain/src/main/java/com/a6w/memo/domain/model/Mapmo.kt
@@ -1,12 +1,23 @@
 package com.a6w.memo.domain.model
-
+/**
+ * Mapmo model
+ * - Detailed information of Mapmo
+ * - Available on the memo detail page
+ */
 data class Mapmo(
+    // Mapmo unique Id
     val mapmoId : String,
+    // Mapmo title & content
     val content: String,
+    // Mapmo notification settings status
     val isNotifyEnabled : Boolean,
+    // Label name of the Mapmo
     val labelName: String,
+    // Label icon color of the Mapmo
     val labelColor: String,
+    // Location of Mapmo
     val location : Location,
+    // Last modified date and time
     val updatedAt : Long,
 )
 
