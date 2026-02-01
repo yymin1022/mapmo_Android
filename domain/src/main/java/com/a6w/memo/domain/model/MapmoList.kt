f73e5154d1feb336c34291ebcb19701b3821e267
Lim-shweety <smile70200@naver.com>
2026-02-01 17:57:24 +0900

    Update MapmoList.kt : Add comments

--- domain/src/main/java/com/a6w/memo/domain/model/MapmoList.kt
+++ domain/src/main/java/com/a6w/memo/domain/model/MapmoList.kt
@@ -1,7 +1,13 @@
 package com.a6w.memo.domain.model
 
+/**
+ * MapmoList model
+ * - Full list of Mapmos
+ */
 data class MapmoList(
+    // Number of Mapmo
     val count: Int,
+    // List of Mapmo of each Label
     val list: List<MapmoListItem>,
 )
 
