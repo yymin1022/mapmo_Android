01fe45422bc7a6b3f6e5174f9823fc2bf51410ed
yymin1022 <yymin1022@gmail.com>
2026-02-03 23:29:18 +0900

    Added KakaoMapView Composable

---
+++ app/src/main/java/com/a6w/memo/common/ui/KakaoMapView.kt
@@ -0,0 +1,15 @@
+package com.a6w.memo.common.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Kakao Map View
+ * - Render KakaoMap View Instance as Composable AndroidView
+ */
+@Composable
+fun KakaoMapView(
+    modifier: Modifier = Modifier,
+) {
+    // TODO: Implement KakaoMap AndroidView Instance
+}
