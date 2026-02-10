b7c2e2526c94869e4fab37ff49e47a66da43a6d7
Lim-shweety <smile70200@naver.com>
2026-02-11 00:02:18 +0900

    Updated LabelRepository.kt: Apply code review feedback - code refactor

--- domain/src/main/java/com/a6w/memo/domain/repository/LabelRepository.kt
+++ domain/src/main/java/com/a6w/memo/domain/repository/LabelRepository.kt
@@ -9,13 +9,13 @@ import com.a6w.memo.domain.model.LabelList
  */
 interface LabelRepository {
     // Fetch all label list by userID
-    suspend fun getLabelList(userID:String): LabelList?
+    suspend fun getLabelList(userID: String): LabelList?
     // Fetch label list by userID & labelID
-    suspend fun getLabel(labelID:String, userID:String):Label?
+    suspend fun getLabel(labelID: String, userID: String): Label?
     // Add mapmo Label
-    suspend fun addLabel(userID : String, labelContent: Label): Boolean
+    suspend fun addLabel(userID: String, labelContent: Label): Boolean
     // update mapmo label
-    suspend fun updateLabel(labelID:String, labelContent :Label):Boolean
+    suspend fun updateLabel(labelID: String, labelContent: Label): Boolean
     // Delete mapmo label
-    suspend fun deleteLabel(labelID:String, userID:String):Boolean
+    suspend fun deleteLabel(labelID: String, userID: String): Boolean
 }
