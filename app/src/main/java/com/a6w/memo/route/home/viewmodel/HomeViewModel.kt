5739e92a87424490659d4615188cd5a19f78b176
yymin1022 <yymin1022@gmail.com>
2026-02-22 17:16:15 +0900

    Update Home ViewModel : Structured initialize functions

--- app/src/main/java/com/a6w/memo/route/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/a6w/memo/route/home/viewmodel/HomeViewModel.kt
@@ -2,9 +2,12 @@ package com.a6w.memo.route.home.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.a6w.memo.data.repository.MapmoListRepositoryImpl
+import com.a6w.memo.common.model.MapCameraFocusData
+import com.a6w.memo.common.model.MapMarkerData
+import com.a6w.memo.domain.model.Mapmo
+import com.a6w.memo.domain.model.MapmoList
 import com.a6w.memo.domain.repository.MapmoListRepository
-import com.a6w.memo.domain.repository.MapmoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -14,10 +17,9 @@ import javax.inject.Inject
  * Home ViewModel Class
  * - Manage states of Home Screen
  */
-class HomeViewModel
-@Inject constructor(
+@HiltViewModel
+class HomeViewModel @Inject constructor(
     private val mapmoListRepository: MapmoListRepository,
-    private val mapmoRepository: MapmoRepository
 ): ViewModel() {
     companion object {
         // TODO: User ID must be managed with User Info
@@ -28,18 +30,83 @@ class HomeViewModel
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState = _uiState.asStateFlow()
 
+    init {
+        viewModelScope.launch {
+            fetchInitialUiState()
+        }
+    }
+
+    /**
+     * Move camera focus to Mapmo
+     */
+    fun moveMapCameraToMapmo(mapmo: Mapmo?) {
+        if(mapmo == null) return
+
+        // Get mapmo location info
+        val mapmoLat = mapmo.location.lat.toFloat()
+        val mapmoLng = mapmo.location.lng.toFloat()
+
+        // Generate camera focus data
+        val cameraFocusData = MapCameraFocusData(
+            latitude = mapmoLat,
+            longitude = mapmoLng,
+        )
+
+        // Update as UI State
+        _uiState.value = _uiState.value.copy(
+            mapCameraFocus = cameraFocusData,
+        )
+    }
+
+    /**
+     * Generate init state
+     */
+    private suspend fun fetchInitialUiState() {
+        // Initialize mapmo list state
+        val mapmoList = getMapmoList()
+        val mapMarkerList = getMapMarkerList(mapmoList)
+
+        // Update UI State
+        _uiState.value = _uiState.value.copy(
+            mapmoList = mapmoList,
+            mapMarkerList = mapMarkerList,
+        )
+    }
+
     /**
      * Get mapmo list instance and save as ui state
      */
-    fun getMapmoList() {
-        viewModelScope.launch {
-            // Get mapmo list from repository
-            val mapmoList = mapmoListRepository.getMapmoList(TEST_USER_ID)
+    private suspend fun getMapmoList(): MapmoList? {
+        // Get mapmo list from repository
+        val mapmoList = mapmoListRepository.getMapmoList(TEST_USER_ID)
 
-            // Update as UI State
-            _uiState.value = _uiState.value.copy(
-                mapmoList = mapmoList,
-            )
+        return mapmoList
+    }
+
+    /**
+     * Generate map markers from mapmo list
+     */
+    private fun getMapMarkerList(
+        mapmoList: MapmoList?,
+    ): List<MapMarkerData>? {
+        // Generate marker list from mapmo list instance
+        val markerList = mapmoList?.list?.flatMap { listItem ->
+            listItem.mapmoList.map { mapmo ->
+                val mapmoLocation = mapmo.location
+                val mapmoLocationLat = mapmoLocation.lat.toFloat()
+                val mapmoLocationLng = mapmoLocation.lng.toFloat()
+                val mapmoTitle = mapmo.content
+
+                val markerData = MapMarkerData(
+                    latitude = mapmoLocationLat,
+                    longitude = mapmoLocationLng,
+                    markerTitle = mapmoTitle,
+                )
+
+                markerData
+            }
         }
+
+        return markerList
     }
 }
