6d6694070e73aae83617ad0ab411c22c0c294b25
yymin1022 <yymin1022@gmail.com>
2026-02-25 21:57:04 +0900

    Update Mapmo ViewModel : Added temp code for mapmo ID debugging

--- app/src/main/java/com/a6w/memo/route/mapmo/MapmoViewModel.kt
+++ app/src/main/java/com/a6w/memo/route/mapmo/MapmoViewModel.kt
@@ -1,13 +1,24 @@
 package com.a6w.memo.route.mapmo
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.toRoute
+import com.a6w.memo.navigation.MapmoNavRoute
 import javax.inject.Inject
 
 /**
  * Mapmo ViewModel Class
  * - Manage states of Mapmo Screen
  */
-class MapmoViewModel
-@Inject constructor(): ViewModel() {
+class MapmoViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
     // TODO: Mapmo ViewModel Instance Setup
+
+    // TODO: Temporary code for mapmo ID Debugging
+    private val route = savedStateHandle.toRoute<MapmoNavRoute.Mapmo>()
+    init {
+        val mapmoID = route.mapmoID
+        println(mapmoID)
+    }
 }
