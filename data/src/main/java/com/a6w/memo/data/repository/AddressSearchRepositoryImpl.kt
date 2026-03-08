d9b84f6062debbc1b11d66f94f082de7e7f2cf6b
yymin1022 <yymin1022@gmail.com>
2026-03-08 23:37:04 +0900

    Update AddressSearchRepository Implementation : Added implementation with Retrofit Data Source

--- data/src/main/java/com/a6w/memo/data/repository/AddressSearchRepositoryImpl.kt
+++ data/src/main/java/com/a6w/memo/data/repository/AddressSearchRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.a6w.memo.data.repository
 
+import com.a6w.memo.data.retrofit.model.AddressMapper.toDomain
+import com.a6w.memo.data.retrofit.source.RetrofitAddressDataSource
 import com.a6w.memo.domain.model.Address
 import com.a6w.memo.domain.repository.AddressSearchRepository
+import javax.inject.Inject
 
 /**
  * AddressSearchRepository Implementation
@@ -9,10 +12,19 @@ import com.a6w.memo.domain.repository.AddressSearchRepository
  * - Repository implementation for address searching with keyword
  * - It uses Kakao Local API for Address Searching
  */
-class AddressSearchRepositoryImpl: AddressSearchRepository {
+class AddressSearchRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RetrofitAddressDataSource
+) : AddressSearchRepository {
     // Fetch address search results with [keyword]
     override suspend fun getSearchResult(keyword: String): List<Address> {
-        // TODO: Implement Kakao Local API Fetching
-        return emptyList()
+        return try {
+            // Call API with Data Source
+            val response = remoteDataSource.fetchAddresses(keyword)
+            // Map Kakao Local Result as Address
+            response.documents.map { it.toDomain() }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptyList()
+        }
     }
 }
