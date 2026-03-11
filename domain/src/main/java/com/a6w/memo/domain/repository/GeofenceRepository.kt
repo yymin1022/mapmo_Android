762223c144a0940b1702e096829ceab3da598bd2
yymin1022 <yymin1022@gmail.com>
2026-03-11 23:55:14 +0900

    Update GeofenceRepository Interface : Added method definition

--- domain/src/main/java/com/a6w/memo/domain/repository/GeofenceRepository.kt
+++ domain/src/main/java/com/a6w/memo/domain/repository/GeofenceRepository.kt
@@ -1,4 +1,15 @@
 package com.a6w.memo.domain.repository
 
+import com.a6w.memo.domain.model.Location
+
+/**
+ * Geofence Repository
+ * - Manage geofence operations
+ * - Add / Remove geofencing to Android GMS
+ */
 interface GeofenceRepository {
+    // Register geofencing
+    fun registerGeofence(mapmoID: String, location: Location): Result<Unit>
+    // Unregister geofencing
+    fun unregisterGeofence(mapmoID: String): Result<Unit>
 }
