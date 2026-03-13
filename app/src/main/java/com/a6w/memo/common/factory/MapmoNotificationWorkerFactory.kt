234d99f67ecd82a1692fd80b9c0bee129da34836
yymin1022 <yymin1022@gmail.com>
2026-03-13 18:28:48 +0900

    Added MapmoNotificationWorker Factory

---
+++ app/src/main/java/com/a6w/memo/common/factory/MapmoNotificationWorkerFactory.kt
@@ -0,0 +1,7 @@
+package com.a6w.memo.common.factory
+
+/**
+ * Mapmo Notification Worker Factory
+ * - Generates instance of [MapmoNotificationWorker] with Android Context
+ */
+class MapmoNotificationWorkerFactory
