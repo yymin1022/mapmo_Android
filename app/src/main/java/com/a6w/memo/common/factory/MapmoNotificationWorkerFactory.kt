9384ce899f8776c10b5848ab987aab59400bd235
yymin1022 <yymin1022@gmail.com>
2026-03-13 18:45:10 +0900

    Update MapmoNotificationWorker Factory : Implemented factory logic

--- app/src/main/java/com/a6w/memo/common/factory/MapmoNotificationWorkerFactory.kt
+++ app/src/main/java/com/a6w/memo/common/factory/MapmoNotificationWorkerFactory.kt
@@ -1,7 +1,41 @@
 package com.a6w.memo.common.factory
 
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.a6w.memo.data.worker.MapmoNotificationWorker
+import com.a6w.memo.domain.repository.MapmoRepository
+import com.a6w.memo.domain.service.MapmoNotificationService
+import javax.inject.Inject
+import kotlin.jvm.java
+
 /**
  * Mapmo Notification Worker Factory
  * - Generates instance of [MapmoNotificationWorker] with Android Context
  */
-class MapmoNotificationWorkerFactory
+class MapmoNotificationWorkerFactory @Inject constructor(
+    private val repository: MapmoRepository,
+    private val notificationService: MapmoNotificationService
+): WorkerFactory() {
+    // Create Mapmo Notification Worker
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters,
+    ): ListenableWorker? {
+        // Generate Mapmo Notification Worker
+        // - If class name is other, nothing is generated
+        return when(workerClassName) {
+            MapmoNotificationWorker::class.java.name -> {
+                MapmoNotificationWorker(
+                    context = appContext,
+                    params = workerParameters,
+                    repository = repository,
+                    notificationService = notificationService,
+                )
+            }
+            else -> null
+        }
+    }
+}
