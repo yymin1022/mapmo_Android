066a3c98d7de5efdda368cfa2ac91b9ba2d02aae
yymin1022 <yymin1022@gmail.com>
2026-03-14 17:36:18 +0900

    Update DI : Added Geofence Repository Declaration

--- app/src/main/java/com/a6w/memo/di/RepositoryModule.kt
+++ app/src/main/java/com/a6w/memo/di/RepositoryModule.kt
@@ -1,11 +1,15 @@
 package com.a6w.memo.di
 
+import android.app.PendingIntent
+import com.a6w.memo.data.repository.GeofenceRepositoryImpl
 import com.a6w.memo.data.repository.LabelRepositoryImpl
 import com.a6w.memo.data.repository.MapmoListRepositoryImpl
 import com.a6w.memo.data.repository.MapmoRepositoryImpl
+import com.a6w.memo.domain.repository.GeofenceRepository
 import com.a6w.memo.domain.repository.LabelRepository
 import com.a6w.memo.domain.repository.MapmoListRepository
 import com.a6w.memo.domain.repository.MapmoRepository
+import com.google.android.gms.location.GeofencingClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +19,13 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideGeofenceRepository(
+        geofencingClient: GeofencingClient,
+        geofencePendingIntent: PendingIntent,
+    ): GeofenceRepository = GeofenceRepositoryImpl(geofencingClient, geofencePendingIntent)
+
     @Provides
     @Singleton
     fun provideMapmoRepository(): MapmoRepository = MapmoRepositoryImpl()
