7659904f2b93ba368f6e7d96cb93303468c99796
yymin1022 <yymin1022@gmail.com>
2026-03-14 16:55:43 +0900

    Update MapmoApplication : Added worker configuration

--- app/src/main/java/com/a6w/memo/MapmoApplication.kt
+++ app/src/main/java/com/a6w/memo/MapmoApplication.kt
@@ -1,11 +1,25 @@
 package com.a6w.memo
 
 import android.app.Application
+import androidx.work.Configuration
+import com.a6w.memo.common.factory.MapmoNotificationWorkerFactory
 import com.a6w.memo.common.util.KakaoMapUtil
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MapmoApplication: Application() {
+class MapmoApplication: Application(), Configuration.Provider {
+    // Mapmo Notification Worker Factory
+    // - Inject by Hilt
+    // - Generate worker configuration
+    @Inject
+    lateinit var workerFactory: MapmoNotificationWorkerFactory
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(android.util.Log.DEBUG)
+            .build()
+
     override fun onCreate() {
         super.onCreate()
 
